bb703a799fb3d8142d7801acc6a9a842e67397a9
Muhammad Rafly Purnama <raflypurnama535@gmail.com>
2022-12-07 18:48:07 +0700

    bismillah bener

--- utility.sh
+++ utility.sh
@@ -1,6 +1,47 @@
 #!/bin/bash
 
-show_menu()
+function pause(){
+    local message="$@"
+    [ -z $message ] && message="Press [Enter] key to continue..."
+    read -p "$message" readEnterKey
+}
+
+# Tampilan main menu
+function show_menu(){
+    echo "---------------------------"
+    echo " Main Menu"
+    echo "---------------------------"
+    echo "1. Who is online"
+    echo "12. Exit"
+}
+
+# Tampilan header pada pesan
+function write_header(){
+    local h="$@"
+    echo "---------------------------------------------------------------"
+    echo " ${h}"
+    echo "---------------------------------------------------------------"
+}
+
+# Tampilan daftar pengguna yang login
+function user_info(){
+    local cmd="$1"
+    case "$cmd" in 
+    who) write_header " Who is online "; who -H; pause ;;
+    esac 
+}
+
+function read_input(){
+    local c
+    read -p "Masukkan Pilihan [ 1 - 12 ] : " c
+    case $c in
+    1) user_info "who" ;;
+    12) echo "Bye!"; exit 0 ;;
+    *) 
+    echo "Pilih antara 1 - 12 saja."
+    pause
+    esac
+}
 
 while true
     do
