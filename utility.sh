6734dea2e1956c8df6628282f2c4d201fcface0d
oxidices <ndaffaraihan@gmail.com>
2022-12-09 20:53:43 +0700

    added simple calculator feature

--- utility.sh
+++ utility.sh
@@ -9,15 +9,17 @@ function pause(){
 # Tampilan main menu
 function show_menu(){
     echo "---------------------------"
-    echo " Main Menu"
+    echo "         Main Menu"
     echo "---------------------------"
     echo "1. Who is online"
+    echo "2. Simple Calculator"
     echo "12. Exit"
 }
 
 # Tampilan header pada pesan
 function write_header(){
     local h="$@"
+    clear
     echo "---------------------------------------------------------------"
     echo " ${h}"
     echo "---------------------------------------------------------------"
@@ -27,7 +29,7 @@ function write_header(){
 function user_info(){
     local cmd="$1"
     case "$cmd" in 
-    who) write_header " Who is online "; who -H; pause ;;
+    who) write_header "        Who is online"; who -H; pause ;;
     esac 
 }
 
@@ -36,11 +38,49 @@ function read_input(){
     read -p "Masukkan Pilihan [ 1 - 12 ] : " c
     case $c in
     1) user_info "who" ;;
+    2) simple_calculator ;;
     12) echo "Bye!"; exit 0 ;;
     *) 
     echo "Pilih antara 1 - 12 saja."
     pause
     esac
+}
+
+function simple_calculator() {
+    local num1
+    local num2
+    local choice
+    local result
+
+    write_header "        Simple Calculator"
+
+    # input 2 angka
+    echo "Masukkan 2 angka : "
+    read num1
+    read num2
+    
+    # input operasi perhitungan 
+    echo "Operasi Perhitungan :"
+    echo "1. Penjumlahan"
+    echo "2. Pengurangan"
+    echo "3. Perkalian"
+    echo "4. Pembagian"
+    echo -n "Masukkan pilihan [1 - 4] : "
+    read choice
+    
+    # switch case perhitungan
+    case $choice in
+        1)result=$(expr $num1 + $num2) ;;
+        2)result=$(expr $num1 - $num2) ;;
+        3)result=$(expr $num1 \* $num2) ;;
+        4)result=$(expr $num1 / $num2) ;;
+        *)
+        echo "Hanya pilih 1 - 4!" ;;
+    esac
+                                                            
+    echo -n "Hasil perhitungan --> "
+    echo $result
+    pause
 }
 
 while true
